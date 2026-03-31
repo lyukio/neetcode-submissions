47f0dc9d8012c5d407d1d9338c567406ac312604
Leonardo Yukio <lyukio@hotmail.com>
2026-03-31 00:10:28 -0300

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        console.log(Math.floor(2.5))
+        let l = 0,
+            r = nums.length
+
+        while (l < r) {
+            const i = l + Math.floor((r - l) / 2)
+            
+            if (target === nums[i]) return i
+            if (target > nums[i]) {
+                l = i + 1
+            } else {
+                r = i
+            }
+        }
+
+        return l < nums.length && nums[l] === target ? l : -1;
+    }
+}
